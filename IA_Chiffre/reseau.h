a06c7ee4d7093393871d2ed5ad21c39a546ecb90
alexr <alex.rousseau77130@gmail.com>
2021-05-14 09:40:54 +0200

    maj structure

--- IA_Chiffre/reseau.h
+++ IA_Chiffre/reseau.h
@@ -1,14 +1,13 @@
 #pragma once
-// 07/05/2021 Anthony Leblanc, Alex ROUSSEAU
+// dernière modification 14/05/2021 Anthony Leblanc, Alex ROUSSEAU
 
 /*
 Définition des structures des réseaux de neuronnes
-1 : une structure couches contenant une liste de neuronne et une structure 
-2 : une structure neurone contenant weight (w) et le biais b
-3 : une structure list contenant un pointeur vers un pointeur d'entrée et un pointeur vers un buffer de sortie
+1 : une structure couche contenant une liste de neuronne, les pointeurs sur la couche précédente et la couche d'avant ainsi que des buffer d'entrée/sortie
+2 : une structure neurone contenant weight (w), le biais b et deux pointeur un sur le neurone précédent et un sur le suivant 
+3 : une structure list contenant un pointeur vers le neurone du début de liste et le neurone de fin de liste
 */
 
-// #define N 784 pas sur à changer peut être 
 
 typedef struct _LIST
 {
@@ -18,13 +17,18 @@ typedef struct _LIST
 
 typedef struct _COUCHE
 {
-  LIST nerone;
+  LIST neurone;
   struct _COUCHE* next;
   struct _COUCHE* prev;
+  double **in;
+  double **out;
+
 }COUCHE;
 
 typedef struct _NEURONE
 {
   unsigned int biais; 
   unsigned int *weight; 
+  struct _NEURONE* next;
+  struct _NEURONE* prev;
 }NEURONE;
