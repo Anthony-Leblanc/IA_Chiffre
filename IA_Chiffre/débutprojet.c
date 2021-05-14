6a68b89683bb1b8bd2f8204d2d63beb052b5187c
alexr <alex.rousseau77130@gmail.com>
2021-05-14 11:17:49 +0200

    Change

--- IA_Chiffre/débutprojet.c
+++ IA_Chiffre/débutprojet.c
@@ -1,1 +1,25 @@
-// 07/05/2021 Alex ROUSSEAU binome (Anthony Leblanc)
+#define _CRT_SECURE_NO_WARNINGS
+
+/*
+Leblanc Anthony, Rousseau Alex
+
+Dernière modification : 14/05/2021*/
+
+#include "reseau.h"
+#include<stdio.h> /*printf*/
+
+void test1(void);
+
+int main(int argc, char argv[])
+{
+  void test1();
+}
+
+void test1(void)
+{
+ int tab[] = { 0,1,2,3,4,5,6,7,8,9 };
+ NEURONE neurone;
+ neurone.biais = 1;
+ neurone.weight = tab;
+ printNEURONE(neurone);
+}
